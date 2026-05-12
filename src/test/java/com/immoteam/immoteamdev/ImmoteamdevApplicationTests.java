3650819a3ccdd7a915d83692aaa4c8f14a9ee086
einsdev <tagnewilfried4@gmail.com>
2026-05-12 14:43:27 +0100

    springBoot initialized and dependencies added

---
+++ src/test/java/com/immoteam/immoteamdev/ImmoteamdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immoteam.immoteamdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmoteamdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
