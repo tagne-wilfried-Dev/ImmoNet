9d1d7c6224714ad80760204be7bffa8952c3ae74
einsdev <tagnewilfried4@gmail.com>
2026-05-12 14:43:27 +0100

    springBoot initialized and dependencies added

---
+++ src/main/java/com/immoteam/immoteamdev/ImmoteamdevApplication.java
@@ -0,0 +1,13 @@
+package com.immoteam.immoteamdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImmoteamdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImmoteamdevApplication.class, args);
+	}
+
+}
