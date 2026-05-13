7b7006d6aa8e2881e952121c26a7b7ff1562ddba
einsdev <tagnewilfried4@gmail.com>
2026-05-13 11:52:44 +0100

    testte remote

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -43,7 +43,7 @@ function App() {
             <li>
               <a href="https://vite.dev/" target="_blank">
                 <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
+                Explore timi
               </a>
             </li>
             <li>
