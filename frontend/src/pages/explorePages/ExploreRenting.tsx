e63cd9e77b14b47374687da45c6e87202ca05985
einsdev <tagnewilfried4@gmail.com>
2026-05-25 15:57:25 +0100

    first pages ended

---
+++ frontend/src/pages/explorePages/ExploreRenting.tsx
@@ -0,0 +1,7 @@
+import ExplorePage from "@/components/explore/ExplorePage";
+
+export default function ExploreRenting() {
+    return (
+        <ExplorePage title="Decouvrez les biens Immobiliers à louer" mode="louer"/>
+    )
+}
