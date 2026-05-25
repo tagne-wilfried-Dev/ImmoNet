50a4855727e816744b6fd286c857017fd6f3cff8
einsdev <tagnewilfried4@gmail.com>
2026-05-25 15:57:25 +0100

    first pages ended

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,33 @@
 
 import './App.css'
+import Footer from './components/layout/Footer'
+import Header from './components/layout/Header'
+import ExploreSellings from './pages/explorePages/ExploreSellings';
+import ExploreRenting from './pages/explorePages/ExploreRenting';
+import { useState } from 'react';
+
+/* pour la gestion des liens de base */
+type ExploreType = 'rent' | 'sell';
 
 function App(){
-  
-  return ()
+  const [currentExplore, setCurrentExplore] = useState<ExploreType>('rent');
+
+  let content;
+  if(currentExplore === 'rent') {
+    content = <ExploreRenting />;
+  } else if(currentExplore === 'sell') {
+    content = <ExploreSellings />;
+  }
+  return (
+    <>
+      <Header currentExplore={currentExplore}
+      onNavigate={setCurrentExplore} />
+      <main className="container mx-auto px-6 py-12">
+        {content}
+      </main>
+      {/* Footer */}
+      <Footer />
+    </>
+  )
 }
 export default App
