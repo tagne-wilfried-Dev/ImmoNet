d11d286e58ca88863dd732d7dc32bc7498c48708
einsdev <tagnewilfried4@gmail.com>
2026-05-31 22:16:21 +0100

    feat: authentification register valide teste

---
+++ backend/src/main/java/com/immoteam/immoteamdev/dto/LoginRequest.java
@@ -0,0 +1,18 @@
+package com.immoteam.immoteamdev.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @Email(message = "Email invalide")
+    @NotBlank(message = "L'email est obligatoire")
+    private String email;
+
+    @NotBlank(message = "Le mot de passe est obligatoire")
+    private String motDePasse;
+}
