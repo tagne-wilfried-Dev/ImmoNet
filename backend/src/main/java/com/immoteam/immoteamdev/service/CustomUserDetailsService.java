00aa93681cceb29169a86d3db497a341ff6eecbc
einsdev <tagnewilfried4@gmail.com>
2026-06-03 22:09:59 +0100

    end authentication basic and user services

--- backend/src/main/java/com/immoteam/immoteamdev/service/CustomUserDetailsService.java
+++ backend/src/main/java/com/immoteam/immoteamdev/service/CustomUserDetailsService.java
@@ -1,14 +1,11 @@
 package com.immoteam.immoteamdev.service;
 
-import org.springframework.security.authentication.DisabledException;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.immoteam.immoteamdev.entity.Utilisateur;
-import com.immoteam.immoteamdev.entity.enums.StatutUtilisateur;
 import com.immoteam.immoteamdev.repository.UtilisateurRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -23,15 +20,7 @@ public class CustomUserDetailsService implements UserDetailsService {
         Utilisateur utilisateur = utilisateurRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("Aucun Utilisateur corespondant a : " + email));
 
-        if (!StatutUtilisateur.ACTIVE.equals(utilisateur.getStatut())) {
-            throw new DisabledException("Compte désactivé");
-        }
 
-        return User.builder()
-                .username(utilisateur.getEmail())
-                .password(utilisateur.getMotDePasseHash())
-                .roles(utilisateur.getRole().name())
-                .disabled(!utilisateur.isEmailVerifie()) // Optionnel : forcer vérification email
-                .build();
+        return new UtilisateurPrincipal(utilisateur);
     }
 }
